95c5f95352bf4e6896536be33a736dd6cfd340d8
ABDALRAHMAN <abdalrahmandaas963@gmail.com.com>
2022-11-25 21:10:02 +0200

    up

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,19 @@
-import zero from "./assets/numbers/0.jpg";
-import one from "./assets/numbers/1.jpg";
-import two from "./assets/numbers/2.jpg";
-import three from "./assets/numbers/3.jpg";
-import four from "./assets/numbers/4.jpg";
-import five from "./assets/numbers/5.jpg";
-import six from "./assets/numbers/6.jpg";
-import seven from "./assets/numbers/7.jpg";
-import eight from "./assets/numbers/8.jpg";
-import nine from "./assets/numbers/9.jpg";
+import zero from "./assets/numbers2/0.png";
+import one from "./assets/numbers2/1.png";
+import two from "./assets/numbers2/2.png";
+import three from "./assets/numbers2/3.png";
+import four from "./assets/numbers2/4.png";
+import five from "./assets/numbers2/5.png";
+import six from "./assets/numbers2/6.png";
+import seven from "./assets/numbers2/7.png";
+import eight from "./assets/numbers2/8.png";
+import nine from "./assets/numbers2/9.png";
+
+import ten from "./assets/numbers2/10.png";
+import one_one from "./assets/numbers2/11.png";
+import one_two from "./assets/numbers2/12.png";
+import one_three from "./assets/numbers2/13.png";
+import one_four from "./assets/numbers2/14.png";
 
 const numbers = {
   zero,
@@ -20,9 +26,15 @@ const numbers = {
   seven,
   eight,
   nine,
+  ten,
+  one_one,
+  one_two,
+  one_three,
+  one_four,
 };
 
 export const grids = [
+  { id: 0, img: null },
   { id: 1, img: null },
   { id: 2, img: null },
   { id: 3, img: null },
@@ -33,10 +45,15 @@ export const grids = [
   { id: 8, img: null },
   { id: 9, img: null },
   { id: 10, img: null },
+  { id: 11, img: null },
+  { id: 12, img: null },
+  { id: 13, img: null },
+  { id: 14, img: null },
 ];
 
 export const cards = [
   // ? { id: 1, img: "url", solved: false, has: {} },
+  { id: 0, img: numbers.zero, solved: false },
   { id: 1, img: numbers.one, solved: false },
   { id: 2, img: numbers.two, solved: false },
   { id: 3, img: numbers.three, solved: false },
@@ -46,7 +63,11 @@ export const cards = [
   { id: 7, img: numbers.seven, solved: false },
   { id: 8, img: numbers.eight, solved: false },
   { id: 9, img: numbers.nine, solved: false },
-  { id: 10, img: numbers.zero, solved: false },
+  { id: 10, img: numbers.ten, solved: false },
+  { id: 11, img: numbers.one_one, solved: false },
+  { id: 12, img: numbers.one_two, solved: false },
+  { id: 13, img: numbers.one_three, solved: false },
+  { id: 14, img: numbers.one_four, solved: false },
 ];
 
 export const hande = {
